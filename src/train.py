b127f9429d342e570752cd464ae5e9e32aa27360
aljaziz <turjatheharbingerofdeath@gmail.com>
2025-06-30 15:33:22 +0600

    training pipeline completed

--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+import mlflow.sklearn
+import mlflow.sklearn
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import pickle
@@ -27,7 +29,7 @@ def hyperparameter_tuning(X_train, y_train, param_grid):
     return grid_search
 
 
-def train(data_path, model_path, random_state, n_estiamtors, max_depth):
+def train(data_path, model_path, random_state, n_estiamtors=None, max_depth=None):
     data = pd.read_csv(data_path)
     X = data.drop(columns=["Outcome"])
     y = data["Outcome"]
@@ -54,3 +56,38 @@ def train(data_path, model_path, random_state, n_estiamtors, max_depth):
         y_pred = best_model.predict(X_test, y_test)
         accuracy = accuracy_score(y_test, y_pred)
         print(f"Accuracy: {accuracy}")
+
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_param("best_n_estimators", grid_search.best_params_["n_estimators"])
+        mlflow.log_param("best_max_depth", grid_search.best_params_["max_depth"])
+        mlflow.log_param(
+            "best_min_samples_split", grid_search.best_params_["min_samples_split"]
+        )
+        mlflow.log_param(
+            "best_min_samples_leaf", grid_search.best_params_["min_samples_leaf"]
+        )
+
+        cm = confusion_matrix(y_test, y_pred)
+        cr = classification_report(y_test, y_pred)
+
+        mlflow.log_text(str(cm), "confusion_matrix.txt")
+        mlflow.log_text(cr, "classification_report.txt")
+
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(
+                best_model, "model", registered_model_name="Best Model"
+            )
+        else:
+            mlflow.sklearn.load_model(best_model, "model", signature=signature)
+
+        os.makedirs(os.path.dirname(model_path), exist_ok=True)
+
+        filename = model_path
+        pickle.dump(best_model, open(filename, "wb"))
+        print(f"Model saved to {model_path}")
+
+
+if __name__ == "__main__":
+    train(params["data"], params["model"], params["random,_state"])
